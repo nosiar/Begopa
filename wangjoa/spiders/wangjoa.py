4fc3be00a8ad9325a755698306f42350380bfe43
Jonghwa Woo <wjh0630@gmail.com>
2015-06-08 14:21:21 +0900

    Implement parsing all pages

--- wangjoa/spiders/wangjoa.py
+++ wangjoa/spiders/wangjoa.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from scrapy.spider import Spider
 from scrapy.selector import Selector
@@ -10,15 +12,29 @@ class WangjoaSpider(Spider):
 
     name = 'wangjoa'
     allowed_domains = ['naver.com']
-    start_urls = ['http://blog.naver.com/PostListByTagName.nhn?blogId=begopa1092&logType=0&cpage=18&tagName=%BF%D5%C1%C1%BE%C6']
+    url_format = 'http://blog.naver.com/PostListByTagName.nhn?blogId=begopa1092&logType=0&cpage={0}&tagName=%BF%D5%C1%C1%BE%C6'
+    start_urls = [url_format.format(1)]
 
     def parse(self, response):
         sel = Selector(response)
+
+        page_list = sel.xpath('//table[@class="page-navigation"]/tr/td/a/text()').extract()
+        for page in page_list:
+            yield Request(self.url_format.format(page), callback=self.parse_page)
+      
+        has_next_page = sel.xpath('//table[@class="page-navigation"]/tr/td/a/span[@class="arw"]/text()')[0].extract() == u'▶'
+        if(has_next_page):
+            yield Request(self.url_format.format(int(page_list[-1]) + 1), callback=self.parse, dont_filter=True)
+
+    def parse_page(self, response):
+        sel = Selector(response)
+
         post_list = sel.xpath('//div[@id="tag_list"]/table/tr/td/div/div[@class="list"]')
 
-        print(len(post_list))
         for post in post_list:
             location = post.xpath('p[@class="tit"]/a/text()')[0].extract().split('/')
+            if len(location) < 2:
+                continue
             
             item = WangjoaItem()
             item['url'] = post.xpath('p[@class="tit"]/a/@href')[0].extract();
