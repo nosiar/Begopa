2e1cab6a5ac82139ab9b9515d490e280c3c081e5
Jonghwa Woo <wjh0630@gmail.com>
2015-06-08 15:28:46 +0900

    Add gen_html.py which generates begopa.html from template.html

---
+++ gen_html.py
@@ -0,0 +1,33 @@
+import os
+import json
+from subprocess import call
+
+try:
+    os.remove('wangjoa.json')
+except:
+    pass
+call(['scrapy', 'crawl', '-o', 'wangjoa.json', 'wangjoa'])
+
+with open('wangjoa.json', 'r') as f:
+    post_list = json.loads(f.read())
+
+post_list = sorted(post_list, key=lambda post: (post['name'], post['url']))
+
+post_html_list = [''.join([
+    '<p class="post">\n',
+    '<div class="title">\n',
+    '<span class="name"><a href="{0}">{1}</a></span> / <span class="location">{2}</span>\n'.format(post['url'], post['name'], post['location_detail']),
+    '</div>\n',            
+    '<span class="excerpt" style="font-size:80%;">{0}</span>\n'.format(post['excerpt']),
+    '</p>\n']) for post in post_list]
+
+content = ''.join(post_html_list)
+
+with open('template.html', 'r') as f:
+    html = f.read()
+
+html = html.replace('{{content}}', content)
+
+with open('begopa.html', 'w') as f:
+    f.write(html)
+
